57710f9cc5a78f749ea75398039bb867b09e4132
RomanOnyshchenko <65304695+RomanOnyshchenko@users.noreply.github.com>
2020-06-03 12:16:04 +0300

    Added Entities to DAL

---
+++ ticket_reserve/DAL/Entities/clients.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ticket_reserve.DAL.Entities
+{
+    class clients
+    {
+    }
+}
---
+++ ticket_reserve/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ticket_reserve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
