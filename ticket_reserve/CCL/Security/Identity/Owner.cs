94a1c7e3dc5f674a282ec3747ee9acc8665b6c21
RomanOnyshchenko <65304695+RomanOnyshchenko@users.noreply.github.com>
2020-06-04 16:35:14 +0300

    Added Identity

---
+++ ticket_reserve/CCL/Security/Identity/Owner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Security.Identity
+{
+    public class Owner
+        : seller
+    {
+        public Owner(int sellersId, string name, string login, string password)
+           : base(sellersId, name, login, password)
+        {
+        }
+    }
+}
---
+++ ticket_reserve/CCL/Security/Identity/seller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Security.Identity
+{
+    public abstract class seller
+    {
+        public seller(int sellersId, string name, string login, string password)
+        {
+            SellerID = sellersId;
+            Name = name;
+            Login  = login;
+            Password = password;
+        }
+        public int SellerID { get; }
+        public string Name { get; }
+        public string Login { get; }
+        public string Password { get; }
+    }
+}
