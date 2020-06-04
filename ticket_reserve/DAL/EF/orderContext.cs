e3f8a752ecb97619062023576182531f04d68bca
RomanOnyshchenko <65304695+RomanOnyshchenko@users.noreply.github.com>
2020-06-04 09:27:14 +0300

    Added orderContext

---
+++ ticket_reserve/DAL/EF/orderContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ticket_reserve.DAL.Entities;
+
+namespace DAL.EF
+{
+    public class orderContext
+        : DbContext
+    {
+        public DbSet<ticket> Tickets { get; set; }
+        public DbSet<clients> Clients { get; set; }
+        public DbSet<ticket_order> Ticket_Orders { get; set; }
+        public DbSet<seller> Sellers { get; set; }
+        public orderContext(DbContextOptions options)
+            : base(options) { }
+    }
+}
