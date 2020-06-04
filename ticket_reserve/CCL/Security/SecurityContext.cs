1748d5688150eb26dafbee1dd087971a6c7c24b2
RomanOnyshchenko <65304695+RomanOnyshchenko@users.noreply.github.com>
2020-06-04 16:45:11 +0300

    Added SecurityContext

--- ticket_reserve/CCL/Security/SecurityContext.cs
+++ ticket_reserve/CCL/Security/SecurityContext.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using CCL.Security.Identity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace BLL.Security
+namespace CCL.Security
 {
     public static class SecurityContext
     {
+        static seller _Seller = null;
+
+        public static seller GetSeller()
+        {
+            return _Seller;
+        }
+
+        public static void SetSeller(seller Seller)
+        {
+            _Seller = Seller;
+        }
     }
 }
