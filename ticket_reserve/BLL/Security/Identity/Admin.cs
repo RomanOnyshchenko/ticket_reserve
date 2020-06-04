422cddda58bb5f8e57087bb127649522deabbf22
RomanOnyshchenko <65304695+RomanOnyshchenko@users.noreply.github.com>
2020-06-04 16:30:27 +0300

    Added identity

---
+++ ticket_reserve/BLL/Security/Identity/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Security.Identity
+{
+    public class Admin
+        : seller
+    {
+        public Admin(int sellersId, string name, string login, string password)
+            : base(sellersId, name, login, password)
+        {
+        }
+    }
+}
---
+++ ticket_reserve/BLL/Security/Identity/Owner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Security.Identity
+{
+    public class Owner
+    {
+        public Owner(int sellersId, string name, string login, string password)
+           : base(sellersId, name, login, password)
+        {
+        }
+    }
+}
