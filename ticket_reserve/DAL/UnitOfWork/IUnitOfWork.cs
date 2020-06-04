0b9f9c585a74a092cc67e558d6015dd90d8c1806
RomanOnyshchenko <65304695+RomanOnyshchenko@users.noreply.github.com>
2020-06-04 10:16:51 +0300

    Create IUnitOfWork.cs

---
+++ ticket_reserve/DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IclientRepository clients { get; }
+        IsellerRepository sellers { get; }
+        IticketRepository tickets { get; }
+        IorderRepository orders   { get; }
+        void Save();
+
+    }
+}
